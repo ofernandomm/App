bc82522bdcd09fba2f2d7811d48722e8933e9102
Omar <ofernandomm@gmail.com>
2022-11-07 21:55:37 -0500

    agregando bloqueante cuando no hay nada escrito

--- App.js
+++ App.js
@@ -3,26 +3,30 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   const [textItem,setTextItem]= useState('')
-  const [itemList,setItemList]= useState(['casa','dos','tres'])
+  const [itemList,setItemList]= useState([])
   const onHandlerChangeItem = (text)=>{
     setTextItem(text)
   };
   const addItem=()=>{
-    setItemList(currentItems=>[
-      ...currentItems, textItem
-    ])
-    setTextItem('')
+    if(textItem==''){
+      alert('Agrega una tarea')
+    }else{
+      setItemList(currentItems=>[
+        ...currentItems, {id: Math.random().toString(), value:textItem}
+      ])
+      setTextItem('')
+    }
   }
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
         <TextInput style={styles.input} value={textItem} onChangeText={onHandlerChangeItem}/>
-        <Button title='ADD' color='#113537' onPress={addItem}/>
+        <Button disabled={!textItem} title='ADD' color='#113537' onPress={addItem}/>
       </View>
       <View style={styles.listContainer}>
         <Text style={styles.listTitle}>Todo list</Text>
-        {itemList.map(item=><View key={item} style={styles.listItemContainer}>
-          <Text style={styles.listItem}>{item}</Text>
+        {itemList.map(item=><View key={item.id} style={styles.listItemContainer}>
+          <Text style={styles.listItem}>{item.value}</Text>
         </View>)}
       </View>
     </View>
